fc71e3ddf0a858a79a0fc016f0aeb0cf81faa01d
Brenno-PP <brennopercy@gmail.com>
2023-08-30 17:15:46 -0300

    criação do exercicio smartTv

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,17 @@
+public class Operadores {
+    public static void main(String [] args){
+        int numero = 5;
+        //x repetiçao
+        //numero = numero + 1;
+        numero++;
+
+        System.out.println(numero);
+
+        boolean variavel = true;
+        System.out.println(!variavel); //a variavel principal continua como true
+
+       
+
+    }
+    
+}
