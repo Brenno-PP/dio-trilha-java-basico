7fee6f75c584dee566db77eb4fdc3958151a3681
Brenno-PP <brennopercy@gmail.com>
2023-08-31 18:53:21 -0300

    correção de espaçamento

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -5,19 +5,19 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Olá Cliente! Qual o seu primeiro nome? ");
+        System.out.println("Olá Cliente! Qual o seu primeiro nome?");
         String nome = scanner.next();
 
-        System.out.println("Qual o seu sobrenome? ");
+        System.out.println("Qual o seu sobrenome?");
         String sobrenome = scanner.next();
 
-        System.out.println("Por favor, informe o número da sua agência: ");
+        System.out.println("Por favor, informe o número da sua agência:");
         String agencia = scanner.next();
 
-        System.out.println("Digite agora o número da sua conta: ");
+        System.out.println("Digite agora o número da sua conta:");
         int conta = scanner.nextInt();
 
-        System.out.println("Informe o seu saldo: ");
+        System.out.println("Informe o seu saldo:");
         double saldo = scanner.nextDouble();
 
         System.out.println("Olá " + nome + " " + sobrenome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
